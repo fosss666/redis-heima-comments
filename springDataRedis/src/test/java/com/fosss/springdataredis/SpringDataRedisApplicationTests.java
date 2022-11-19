60c11ded3c32aa84f136f51155ddf4e2ffcfc4ba
fo <1745179058@qq.com>
2022-11-19 21:41:58 +0800

    springDataRedis-序列化

--- springDataRedis/src/test/java/com/fosss/springdataredis/SpringDataRedisApplicationTests.java
+++ springDataRedis/src/test/java/com/fosss/springdataredis/SpringDataRedisApplicationTests.java
@@ -1,5 +1,6 @@
 package com.fosss.springdataredis;
 
+import com.fosss.springdataredis.pojo.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,13 +10,21 @@ import org.springframework.data.redis.core.RedisTemplate;
 class SpringDataRedisApplicationTests {
 
     @Autowired
-    private RedisTemplate redisTemplate;
+//    private RedisTemplate redisTemplate;
+    private RedisTemplate<String,Object> redisTemplate;
 
     @Test
     void testRedis() {
         redisTemplate.opsForValue().set("name","大名");
         Object name = redisTemplate.opsForValue().get("name");
         System.out.println("name = " + name);
+    }
+
+    @Test
+    void testRedis2() {
+        redisTemplate.opsForValue().set("user:100",new User("李四",99));
+        User user = (User) redisTemplate.opsForValue().get("user:100");
+        System.out.println("user = " + user);
     }
 
 }
