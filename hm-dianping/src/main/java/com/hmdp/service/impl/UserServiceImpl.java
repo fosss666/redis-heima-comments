96cf8975657b8ce80b04c26ffe4d81c51be18c8d
fo <1745179058@qq.com>
2022-11-22 21:16:03 +0800

    短信登录-用redis改造发送验证码功能

--- hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -9,10 +9,16 @@ import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.LOGIN_CODE_KEY;
+import static com.hmdp.utils.RedisConstants.LOGIN_CODE_TTL;
 import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
@@ -26,6 +32,9 @@ import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
     // 发送短信验证码并保存验证码
     @Override
     public Result sendCode(String phone, HttpSession session) {
@@ -36,8 +45,11 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
 
         //生成验证码
         String code = RandomUtil.randomNumbers(6);
-        //保存到session中
-        session.setAttribute(phone, code);
+//        //保存到session中
+//        session.setAttribute(phone, code);
+
+        //保存到redis中,并设置过期时间
+        stringRedisTemplate.opsForValue().set(LOGIN_CODE_KEY + phone, code, LOGIN_CODE_TTL, TimeUnit.MINUTES);
 
         //发送短信
         log.debug("验证码发送成功，验证码为：" + code);
