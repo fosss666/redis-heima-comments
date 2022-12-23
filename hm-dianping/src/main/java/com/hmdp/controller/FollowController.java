7341c328f3c3a83bfe947bb73d948c922894b007
fo <1745179058@qq.com>
2022-12-23 20:02:06 +0800

    补充根据id获取粉丝数方法

--- hm-dianping/src/main/java/com/hmdp/controller/FollowController.java
+++ hm-dianping/src/main/java/com/hmdp/controller/FollowController.java
@@ -1,18 +1,35 @@
 package com.hmdp.controller;
 
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.Follow;
+import com.hmdp.service.IFollowService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
- 
  */
 @RestController
 @RequestMapping("/follow")
 public class FollowController {
 
+    @Resource
+    private IFollowService followService;
+
+    /**
+     * 根据id获取粉丝数
+     */
+    @GetMapping("/or/not/{id}")
+    public Result getFollowById(@PathVariable String id) {
+        Follow follow = followService.getById(id);
+        return Result.ok(follow);
+    }
 }
