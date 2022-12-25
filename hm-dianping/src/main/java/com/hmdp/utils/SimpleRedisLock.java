cc3330962b7a5a22d6dcc83d0b1fbd4fc44484c5
fo <1745179058@qq.com>
2022-12-25 19:34:39 +0800

    优惠券秒杀-一人一单并发问题-解决redis分布式锁误删问题

--- hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -2,6 +2,7 @@ package com.hmdp.utils;
 
 import org.springframework.data.redis.core.StringRedisTemplate;
 
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -14,6 +15,7 @@ public class SimpleRedisLock implements ILock {
     private StringRedisTemplate stringRedisTemplate;
     private static final String keyPrefix = "lock:";
     private String name;
+    private static final String lockPrefix = UUID.randomUUID().toString().replace("-", "");
 
     public SimpleRedisLock(StringRedisTemplate stringRedisTemplate, String name) {
         this.stringRedisTemplate = stringRedisTemplate;
@@ -30,7 +32,7 @@ public class SimpleRedisLock implements ILock {
     public boolean tryLock(long timeoutSec) {
         //获取当前线程id作为value
         long threadId = Thread.currentThread().getId();
-        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(keyPrefix + name, threadId + "", timeoutSec, TimeUnit.SECONDS);
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(keyPrefix + name, lockPrefix + "-" + threadId, timeoutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(flag);//防止拆箱时出现空指针问题（flag为空时导致的拆箱空指针）
     }
 
@@ -39,7 +41,12 @@ public class SimpleRedisLock implements ILock {
      */
     @Override
     public void unlock() {
-        stringRedisTemplate.delete(keyPrefix + name);
+        //先判断是不是当前线程的锁，防止误删
+        String lockValue = lockPrefix + Thread.currentThread().getId();
+        String currentLockValue = stringRedisTemplate.opsForValue().get(keyPrefix + name);
+        if (lockValue.equals(currentLockValue)) {
+            stringRedisTemplate.delete(keyPrefix + name);
+        }
     }
 }
 
